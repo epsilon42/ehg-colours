8c90acef72dbeef63794d81ab0b01a8b01df6a01
epsilon42 <andrew@goninon.net>
2020-10-23 13:47:32 +0800

    Updates

---
+++ precache-manifest.f7369113e315de84bd48c0cf8df160dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf686e08a208dc8145615d499e9febd9",
+    "url": "/ehg-colours/index.html"
+  },
+  {
+    "revision": "ea7e4f68b824ce87f03b",
+    "url": "/ehg-colours/static/css/main.70652729.chunk.css"
+  },
+  {
+    "revision": "25cf214b8dc4f1c4c414",
+    "url": "/ehg-colours/static/js/2.fc853ef5.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/ehg-colours/static/js/2.fc853ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7e4f68b824ce87f03b",
+    "url": "/ehg-colours/static/js/main.da120066.chunk.js"
+  },
+  {
+    "revision": "f5d49be4792dee7ea0e0",
+    "url": "/ehg-colours/static/js/runtime-main.511db598.js"
+  }
+]);
